3e5bb90c78a88bb2453094565e67fe161176a894
Sanket-Bharadwaj <sanketbharadwaj15@gmail.com>
2025-02-20 20:59:55 +0530

    Updated

---
+++ 4th-Semester/CSW-2/Assignment-1/Q01/Car.java
@@ -0,0 +1,63 @@
+package Q01;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private double speed;
+
+    // Constructor
+    public Car(String brand, String model, int year, double speed) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.speed = speed;
+    }
+
+    // Getters
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    // Setters
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    // Method to accelerate
+    public void accelerate(double amount) {
+        speed += amount;
+    }
+
+    // Method to brake
+    public void brake(double amount) {
+        speed -= amount;
+        if (speed < 0) {
+            speed = 0;
+        }
+    }
+}
