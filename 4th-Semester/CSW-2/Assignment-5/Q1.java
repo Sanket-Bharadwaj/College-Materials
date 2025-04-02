f51792e3796ffa8860f9dfbac46267f0fcb79720
Sanket Bharadwaj <127912897+Sanket-Bharadwaj@users.noreply.github.com>
2025-04-02 09:30:57 +0530

    Update Q1.java

--- 4th-Semester/CSW-2/Assignment-5/Q1.java
+++ 4th-Semester/CSW-2/Assignment-5/Q1.java
@@ -5,7 +5,19 @@
 
 package Assignment;
 
-public class Q1 {
-	String i="Hello World";
+public class StringDemo {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
 
+        String str3 = new String("Hello");
+        String str4 = new String("Hello");
+
+        System.out.println("Comparing string literals (str1 == str2): " + (str1 == str2)); 
+        System.out.println("Comparing string literal and new String (str1 == str3): " + (str1 == str3)); 
+        System.out.println("Comparing two new String objects (str3 == str4): " + (str3 == str4)); 
+
+        System.out.println("Comparing content of str1 and str3 (str1.equals(str3)): " + str1.equals(str3)); 
+        System.out.println("Comparing content of str3 and str4 (str3.equals(str4)): " + str3.equals(str4)); 
+    }
 }
