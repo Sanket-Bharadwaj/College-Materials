e05e66b99ee034c7a27a710be9f152054a191f6b
Sanket Bharadwaj <127912897+Sanket-Bharadwaj@users.noreply.github.com>
2025-04-02 09:33:57 +0530

    Update Q2.java

--- 4th-Semester/CSW-2/Assignment-5/Q2.java
+++ 4th-Semester/CSW-2/Assignment-5/Q2.java
@@ -5,6 +5,23 @@
 
 package Assignment;
 
-public class Q2 {
+public class StringImmutability {
+    public static void main(String[] args) {
+        // Demonstrating String immutability
+        String str = "Hello";
+        System.out.println("Original String: " + str);
+        
+        // Trying to modify the String (creates a new object instead)
+        String newStr = str.concat(" World");
+        System.out.println("After concatenation: " + newStr);
+        System.out.println("Original String remains unchanged: " + str);
 
+        // Demonstrating CharSequence interface methods
+        CharSequence charSeq = "ImmutableString"; // String implements CharSequence
+
+        System.out.println("CharSequence: " + charSeq);
+        System.out.println("Character at index 5: " + charSeq.charAt(5));
+        System.out.println("Substring (0 to 9): " + charSeq.subSequence(0, 9));
+        System.out.println("Length of CharSequence: " + charSeq.length());
+    }
 }
