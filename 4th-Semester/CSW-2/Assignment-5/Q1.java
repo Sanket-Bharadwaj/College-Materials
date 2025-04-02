8df86b9cfd1554dd2b3043d216354349a9b5f73d
Sanket Bharadwaj <127912897+Sanket-Bharadwaj@users.noreply.github.com>
2025-04-02 09:22:13 +0530

    Add files via upload

---
+++ 4th-Semester/CSW-2/Assignment-5/Q1.java
@@ -0,0 +1,11 @@
+//Write a Java program that illustrates the difference between using 
+//string literals and the new keyword for creating String objects. Your
+//program should demonstrate the memory usage implications and how string 
+//comparison behaves differently in each case.
+
+package Assignment;
+
+public class Q1 {
+	String i="Hello World";
+
+}
